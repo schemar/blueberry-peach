d1ec9f95956242c4e6e0e3940cb91a55ab6409b8
Martin Schenck <martinschenck@fastmail.com>
2026-07-06 11:15:07 +0200

    extract light/dark/mapping into separate file

--- build.lua
+++ build.lua
@@ -1,118 +1,11 @@
 #!/usr/bin/env lua
 
-local blueberry_peach_light = {
-  red = "#C34165",
-  maroon = "#C34165",
-  yellow = "#8A7400",
-  peach = "#AC591C",
-  green = "#288043",
-  teal = "#007E7D",
-  sky = "#007E7D",
-  sapphire = "#007E7D",
-  blue = "#1675AB",
-  lavender = "#1675AB",
-  mauve = "#6A67B4",
-  pink = "#6A67B4",
-  flamingo = "#A352A0",
-  rosewater = "#A352A0",
-  text = "#706F7A",
-  subtext1 = "#757480",
-  subtext0 = "#757480",
-  overlay2 = "#797985",
-  overlay1 = "#7E7D8A",
-  overlay0 = "#84828F",
-  surface2 = "#9C8282",
-  surface1 = "#EBDFD3",
-  surface0 = "#EBDFD3",
-  base = "#FAF4ED",
-  mantle = "#FCF9F5",
-  crust = "#FCF9F5",
-  id = "light",
-  name = "Blueberry Peach Light",
-}
+-- Make nix' lua happy:
+package.path = package.path .. ";./?.lua"
 
-local blueberry_peach_dark = {
-  red = "#DF8BA0",
-  maroon = "#DF8BA0",
-  yellow = "#C7B96F",
-  peach = "#C79A76",
-  green = "#75B087",
-  teal = "#5EB1AF",
-  sky = "#5EB1AF",
-  sapphire = "#5EB1AF",
-  blue = "#7AA8CE",
-  lavender = "#7AA8CE",
-  mauve = "#A19DD4",
-  pink = "#A19DD4",
-  flamingo = "#C394C2",
-  rosewater = "#C394C2",
-  text = "#A2A2A9",
-  subtext1 = "#878794",
-  subtext0 = "#878794",
-  overlay2 = "#7D7D7D",
-  overlay1 = "#808084",
-  overlay0 = "#84848C",
-  surface2 = "#7C7992",
-  surface1 = "#37363E",
-  surface0 = "#37363E",
-  base = "#191724",
-  mantle = "#0B0A0F",
-  crust = "#0B0A0F",
-  id = "dark",
-  name = "Blueberry Peach Dark",
-}
-
-local blueberry_peach_dark_dimmed = {
-  red = "#BF657B",
-  maroon = "#BF657B",
-  yellow = "#91812C",
-  peach = "#A67753",
-  green = "#578D67",
-  teal = "#278F8E",
-  sky = "#278F8E",
-  sapphire = "#278F8E",
-  blue = "#5188A6",
-  lavender = "#5188A6",
-  mauve = "#7E7ABA",
-  pink = "#7E7ABA",
-  flamingo = "#A370A1",
-  rosewater = "#A370A1",
-  text = "#7F7F91",
-  subtext1 = "#797985",
-  subtext0 = "#797985",
-  overlay2 = "#6E6E6E",
-  overlay1 = "#717175",
-  overlay0 = "#76767D",
-  surface2 = "#6A677E",
-  surface1 = "#29263A",
-  surface0 = "#29263A",
-  base = "#191724",
-  mantle = "#0B0A0F",
-  crust = "#0B0A0F",
-  id = "dark-dimmed",
-  name = "Blueberry Peach Dark Dimmed",
-}
-
-local blueberry_peach_palette = {
-  red = "red", -- #C34165
-  yellow = "yellow", -- #8A7400
-  orange = "peach", -- #AC591C
-  green = "green", -- #288043
-  teal = "teal", -- #007E7D
-  blue = "blue", -- #1675AB
-  violet = "mauve", -- #6A67B4
-  pink = "flamingo", -- #A352A0
-  text = "text", -- #5B5B5B
-  subtext = "subtext1", -- #908A84
-  overlay2 = "overlay2", -- #707070
-  overlay1 = "overlay1", -- #646464
-  overlay0 = "overlay0", -- #585858
-  dimmed = "surface2", -- #9C8282
-  surface2 = "surface1", -- #e8dfd4
-  surface1 = "surface0", -- #f1ece5
-  background = "base", -- #faf4ed
-  surface0 = "mantle", -- #FFFAF0
-}
+local blueberry_peach_light = require("lua.light")
+local blueberry_peach_dark = require("lua.dark")
+local map_blueberry_peach_to_catppuccin = require("lua.map_blueberry_peach_catppuccin")
 
 local template_files = {
   {
@@ -207,6 +100,7 @@ local template_files = {
   },
 }
 
+-- Replaces based on catppuccin names in templates:
 for _, template_file in pairs(template_files) do
   local file = io.open(template_file.source, "r")
   if file == nil then
@@ -239,7 +133,7 @@ end
 local content = file:read("*all")
 file:close()
 
-for blueberry_color, catppuccin_color in pairs(blueberry_peach_palette) do
+for blueberry_color, catppuccin_color in pairs(map_blueberry_peach_to_catppuccin) do
   local svg = content:gsub("{{color}}", blueberry_peach_light[catppuccin_color])
   file = io.open("./assets/light_" .. blueberry_color .. ".svg", "w")
   if file == nil then
