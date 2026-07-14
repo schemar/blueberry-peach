8446f42be8dc7c4119a7ba81a7acc1ea0aa0e857
Martin Schenck <martinschenck@fastmail.com>
2026-07-14 22:14:37 +0200

    ports(nvim): add lualine
    Fixes #14

--- scripts/lua/compile_catppuccin_dark.lua
+++ scripts/lua/compile_catppuccin_dark.lua
@@ -4,3 +4,8 @@ local lines = compiler.compile("dark")
 local out = "../ports/neovim/lua/blueberry-peach/dark.lua"
 
 vim.fn.writefile(lines, out)
+
+-- lualine special handling:
+local lualine_out = "../ports/neovim/lua/blueberry-peach/lualine_dark.lua"
+local body = compiler.compile_lualine("dark")
+vim.fn.writefile(vim.split(body, "\n"), lualine_out)
--- scripts/lua/compile_catppuccin_light.lua
+++ scripts/lua/compile_catppuccin_light.lua
@@ -4,3 +4,8 @@ local lines = compiler.compile("light")
 local out = "../ports/neovim/lua/blueberry-peach/light.lua"
 
 vim.fn.writefile(lines, out)
+
+-- lualine special handling:
+local lualine_out = "../ports/neovim/lua/blueberry-peach/lualine_light.lua"
+local body = compiler.compile_lualine("light")
+vim.fn.writefile(vim.split(body, "\n"), lualine_out)
