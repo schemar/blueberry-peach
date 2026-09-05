b61c169e507e44ff163065b309f858d64138ebdf
Martin Schenck <martinschenck@fastmail.com>
2026-09-05 17:25:45 +0200

    ports(fzf): extend `FZF_DEFAULT_OPTS` instead of overwriting

--- ports/fzf/blueberry_peach_dark-fzf-colors.sh
+++ ports/fzf/blueberry_peach_dark-fzf-colors.sh
@@ -1,4 +1,5 @@
-export FZF_DEFAULT_OPTS=" \
+export FZF_DEFAULT_OPTS="\
 --color=bg+:#353144,bg:#191724,spinner:#C394C2,hl:#DF8BA0 \
 --color=fg:#A0A1B6,header:#DF8BA0,info:#A19DD4,pointer:#C394C2 \
---color=marker:#C394C2,fg+:#A0A1B6,prompt:#A19DD4,hl+:#DF8BA0"
+--color=marker:#C394C2,fg+:#A0A1B6,prompt:#A19DD4,hl+:#DF8BA0 \
+${FZF_DEFAULT_OPTS}"
--- templates/fzf/blueberry_peach.sh
+++ templates/fzf/blueberry_peach.sh
@@ -1,4 +1,5 @@
-export FZF_DEFAULT_OPTS=" \
+export FZF_DEFAULT_OPTS="\
 --color=bg+:{{surface1}},bg:{{background}},spinner:{{pink}},hl:{{red}} \
 --color=fg:{{text}},header:{{red}},info:{{violet}},pointer:{{pink}} \
---color=marker:{{pink}},fg+:{{text}},prompt:{{violet}},hl+:{{red}}"
+--color=marker:{{pink}},fg+:{{text}},prompt:{{violet}},hl+:{{red}} \
+${FZF_DEFAULT_OPTS}"
