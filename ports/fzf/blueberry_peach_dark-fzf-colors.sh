0e4f3578ac65853a20f3a9ec7c47685183539246
Martin Schenck <martinschenck@fastmail.com>
2026-09-15 14:00:05 +0200

    palette: adjust contrast outliers (#53)
    Fixes #52

--- ports/fzf/blueberry_peach_dark-fzf-colors.sh
+++ ports/fzf/blueberry_peach_dark-fzf-colors.sh
@@ -1,5 +1,5 @@
 export FZF_DEFAULT_OPTS="\
 --color=bg+:#353144,bg:#191724,spinner:#C394C2,hl:#DF8BA0 \
---color=fg:#A0A1B6,header:#DF8BA0,info:#A69CD9,pointer:#C394C2 \
---color=marker:#C394C2,fg+:#A0A1B6,prompt:#A69CD9,hl+:#DF8BA0 \
+--color=fg:#9FA2BC,header:#DF8BA0,info:#A69CD9,pointer:#C394C2 \
+--color=marker:#C394C2,fg+:#9FA2BC,prompt:#A69CD9,hl+:#DF8BA0 \
 ${FZF_DEFAULT_OPTS}"
